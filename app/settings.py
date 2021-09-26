c078fe90b44d697292a20b201b431efa8c472366
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2021-09-26 03:33:10 +0330

    Liara Deployment.

--- app/settings.py
+++ app/settings.py
@@ -88,9 +88,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # PostgreSQL
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eager_liskov',
+        'USER': 'root',
+        'PASSWORD': 'ZTWejgtwwevvRTGGEvNjpZTF',
+        'HOST': 'nebigdb',
+        'PORT': '3306',
     }
 }
 
