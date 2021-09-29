6eb94cfc10b8a61fe1d037fa425a825cfc2c387a
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2021-09-29 13:29:23 +0330

    Add Admin models.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Translator)
+admin.site.register(Review)
