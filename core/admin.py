ebc18e2fbdd804f7b691290c0b412aa13bcb1423
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2021-10-21 14:35:06 +0330

    Add prepopulated field - slug

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from core.models import *
 
 admin.site.register(UserProfile)
-admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Publisher)
 admin.site.register(Translator)
