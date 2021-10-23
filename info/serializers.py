2c529574c9430cc76604347a97425f957a15fc1c
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2021-10-23 12:36:13 +0330

    Fix serializers name

---
+++ info/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from core.models import About
+
+
+class AboutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields = '__all__'
+        read_only_fields = ('id',)
