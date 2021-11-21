b98ed0b39852d3992278eff14b3d143c4ae4b157
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2021-11-21 14:48:35 +0330

    Add base url

--- info/serializers.py
+++ info/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
+from django.conf import settings
+
 from core.models import About
 
 
 class AboutSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    def get_image(self, obj):
+        base_url = settings.BASE_URL
+        try:
+            return base_url + obj.image.url
+        except:
+            return None
+
     class Meta:
         model = About
         fields = ('title', 'description', 'image',)
