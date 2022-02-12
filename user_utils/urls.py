57384543fee2b72e9fa97f160bfda2453e827709
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2022-02-12 18:11:33 +0330

    Add reset password api

--- user_utils/urls.py
+++ user_utils/urls.py
@@ -6,5 +6,6 @@ app_name = "main"
 
 urlpatterns = [
     path("password_reset", views.password_reset_request, name="password_reset"),
+    path("password_reset_api/", views.PasswordResetAPI.as_view(), name="password_reset_api"),
     path("google_auth/", views.GoolgeAuth.as_view(), name="google_login"),
 ]
--- user_utils/views.py
+++ user_utils/views.py
@@ -13,8 +13,9 @@ from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.contrib import messages
-
 from django.contrib.auth import login, authenticate, logout
+
+from rest_framework.views import APIView
 
 
 def password_reset_request(request):
@@ -43,7 +44,7 @@ def password_reset_request(request):
 						return HttpResponse('Invalid header found.')
 						
 					messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
-					return
+					return HttpResponse("Password reset request sent.")
 			messages.error(request, 'An invalid email has been entered.')
 	password_reset_form = PasswordResetForm()
 	return render(request=request, template_name="main/password/password_reset.html", context={"password_reset_form":password_reset_form})
@@ -52,3 +53,32 @@ def password_reset_request(request):
 class GoolgeAuth(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
     client_class = OAuth2Client
+
+
+class PasswordResetAPI(APIView):
+
+	def post(self, request):
+		email = request.data.get('email').lower()
+		associated_users = User.objects.filter(Q(email=email))
+		if associated_users.exists():
+			for user in associated_users:
+				subject = "Password Reset Requested"
+				email_template_name = "main/password/password_reset_email.txt"
+				c = {
+				"email":user.email,
+				'domain':'nebigapp.com',
+				'site_name': 'Nebig',
+				"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+				'token': default_token_generator.make_token(user),
+				'protocol': 'http',
+				}
+				email = render_to_string(email_template_name, c)
+				try:
+					send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+				except BadHeaderError:
+					return HttpResponse('Invalid header found.')
+				messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+				return HttpResponse("Password reset request sent.")
+		
+		messages.error(request, 'An invalid email has been entered.')		
+		return HttpResponse(status=400)
