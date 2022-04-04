2dbf4473dcd3fa032539234055de2f6193623237
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2022-04-04 17:07:38 +0430

    Redirect to '' to nebigapp.com

--- app/urls.py
+++ app/urls.py
@@ -44,6 +44,8 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Redirect '' to nebigapp.com
+    path('', RedirectView.as_view(url='https://nebigapp.com/', permanent=True)),
     path('admin/', admin.site.urls),
     path('user/' , include('user.urls')),
     path('book/' , include('book.urls')),
