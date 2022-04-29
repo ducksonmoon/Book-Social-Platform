9c2c8c2c85aac8f55d27ecea63dcf9dd8be805f7
Mehrshad Baqerzadegan <newton@Mehrshads-MacBook-Pro.local>
2022-04-29 17:48:57 +0430

    Change main categoryPost url

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('posts/', views.CategoryPostsViewSet.as_view({'get': 'list'}), name='category-posts'),
+    path('categoryPosts/', views.CategoryPostsViewSet.as_view({'get': 'list'}), name='category-posts'),
     path('publishers/', views.PublicationPostsViewSet.as_view({'get': 'list'}), name='publishers'),
 ]
