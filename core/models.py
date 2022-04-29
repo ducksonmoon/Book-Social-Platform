84126be2123f93970c1c65ffb11f2c9afa4a1bd6
Mehrshad Baqerzadegan <newton@Mehrshads-MacBook-Pro.local>
2022-04-29 19:11:07 +0430

    Add banners model

--- core/models.py
+++ core/models.py
@@ -527,3 +527,14 @@ class CategoryPosts(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Baners(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='baners/', blank=True, null=True)
+    related_list = models.ForeignKey(BookList, on_delete=models.CASCADE, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
